5ec73307cbf8977acfc3e8331bb2a80cac192622
luigi-crypto <luisdiegoaguilarrodriguez@gmail.com>
2026-02-26 13:34:08 -0600

    Delete busquedaOrdenamiento/busquedaOrdenamiento/search.h

--- busquedaOrdenamiento/busquedaOrdenamiento/search.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef SEARCH_H
-#define SEARCH_H
-
-// 0/ verbose=true imprime el proceso paso a paso
-int busquedaSecuencial(int arreglo[], int n, int valor, bool verbose = false);
-int busquedaBinaria(int arreglo[], int n, int valor, bool verbose = false);
-
-#endif
